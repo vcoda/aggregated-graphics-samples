21552f9727c8a7d0a93729d364deec9da8b307bf
Victor Coda <victor.coda@gmail.com>
2020-08-07 17:25:34 +0300

    Optimized grid mesh construction: 1) Use single staging buffer for vertex and index buffers. 2) Do not allocate separate half-vertex buffer, do conversion in-place. 3) Get rid of std::vector::push_back().

--- seascape/gridMesh.cpp
+++ seascape/gridMesh.cpp
@@ -1,7 +1,8 @@
 #include "gridMesh.h"
 #include "magma/magma.h"
+#define _XM_F16C_INTRINSICS_
 #include "rapid/DirectXMath/Inc/DirectXPackedVector.h"
-using namespace DirectX;
+using namespace DirectX::PackedVector;
 
 GridMesh::GridMesh(uint16_t rows, uint16_t cols, float scale,
     std::shared_ptr<magma::CommandBuffer> cmdBuffer)
@@ -9,41 +10,47 @@ GridMesh::GridMesh(uint16_t rows, uint16_t cols, float scale,
     const float dx = scale/cols;
     const float dz = scale/rows;
     const float o = -scale * .5f;
-    std::vector<XMFLOAT2> vertices;
-    vertices.reserve((rows + 1) * (cols + 1));
+    const std::size_t vertexBufferSize = (rows + 1) * (cols + 1) * sizeof(XMHALF2);
+    const std::size_t indexBufferSize = rows * (3 + cols * 2) * sizeof(uint16_t);
+    std::shared_ptr<magma::SrcTransferBuffer> stagingBuffer(std::make_shared<magma::SrcTransferBuffer>(
+        cmdBuffer->getDevice(), vertexBufferSize + indexBufferSize));
+    void *data = stagingBuffer->getMemory()->map();
+    XMHALF2 *vert = (XMHALF2 *)data;
     // Setup X, Z coordinates
     float z = o;
     for (uint16_t i = 0, n = rows + 1; i < n; ++i, z += dz)
     {
         float x = o;
         for (uint16_t j = 0, m = cols + 1; j < m; ++j, x += dx)
-            vertices.emplace_back(x, z);
+        {   // Quantize floats to halves
+            vert->x = XMConvertFloatToHalf(x);
+            vert->y = XMConvertFloatToHalf(z);
+            ++vert;
+        }
     }
-    std::vector<uint16_t> indices;
-    indices.reserve(rows * (3 + cols * 2));
+    uint16_t *idx = (uint16_t *)((char *)data + vertexBufferSize);
     const uint16_t stride = cols + 1;
     // Generate indices of triangle strip
     for (uint16_t i = 0; i < rows; ++i)
     {
         const uint16_t first = i * stride;
-        indices.push_back(first);
-        indices.push_back(first + stride);
+        *idx++ = first;
+        *idx++ = first + stride;
         for (uint16_t j = 0, k = 1; j < cols; ++j, ++k)
         {
-            indices.push_back(first + k);
-            indices.push_back(first + k + stride);
-            assert(indices.back() <= std::numeric_limits<uint16_t>::max());
+            *idx++ = first + k;
+            *idx++ = first + k + stride;
+            assert(first + k + stride <= std::numeric_limits<uint16_t>::max());
         }
         // Restart strip
-        indices.push_back(std::numeric_limits<uint16_t>::max());
+        *idx++ = std::numeric_limits<uint16_t>::max();
     }
-    // Quantize floats to halves
-    std::vector<PackedVector::HALF> halfVertices(vertices.size() * 2);
-    PackedVector::XMConvertFloatToHalfStream(halfVertices.data(), sizeof(PackedVector::HALF),
-        (const float *)vertices.data(), sizeof(float), vertices.size() * 2);
     // Create vertex and index buffers
-    vertexBuffer = std::make_shared<magma::VertexBuffer>(cmdBuffer, halfVertices);
-    indexBuffer = std::make_shared<magma::IndexBuffer>(cmdBuffer, indices);
+    vertexBuffer = std::make_shared<magma::VertexBuffer>(cmdBuffer,
+        stagingBuffer, vertexBufferSize, 0);
+    indexBuffer = std::make_shared<magma::IndexBuffer>(cmdBuffer,
+        stagingBuffer, VK_INDEX_TYPE_UINT16, indexBufferSize, vertexBufferSize);
+    stagingBuffer->getMemory()->unmap();
 }
 
 void GridMesh::draw(std::shared_ptr<magma::CommandBuffer> cmdBuffer)
